862290ddc5891edecbc5dfba8fbec7140176c800
MoMawin <KiKissTH@gmail.com>
2019-06-03 22:39:53 +0700

    no message

---
+++ odoo/demo.py
@@ -0,0 +1,18 @@
+import math
+
+def rectangle(w,h):
+    return w*h
+
+def circle(r):
+    return math.pi * r * r
+    a = 7
+
+def main():
+    print(rectangle(5,10))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
